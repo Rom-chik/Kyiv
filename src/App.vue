5d71d4a883ebf6513e722e05be5d4a4246dd1e99
Rom-chik <anr0manking@gmail.com>
2024-08-05 01:33:55 +0300

    First commit. Project setup.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import Navigation from './components/Navigation.vue'
+</script>
+
+<template>
+  <!-- Header -->
+  <div class="flex justify-center items-center w-full h-[40vh] text-9xl">
+      K y i v
+  </div>
+
+  <!-- Navigations Menu -->
+  <div class="flex w-full h-[90vh]">
+
+  </div>
+  <!-- Timestamps -->
+  <div class="flex w-full h-[50vh]">
+
+  </div>
+
+  <!-- Side scroll cards -->
+  <div class="flex w-full h-[100vh] bg-[#F0F0F0]">
+
+  </div>
+
+  <!-- Bottom -->
+  <div class="flex w-full h-[70vh]">
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>

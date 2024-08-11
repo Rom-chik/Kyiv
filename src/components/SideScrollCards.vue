7d2abc27d1feacad22446b8df1c3f90963aa045e
Rom-chik <anr0manking@gmail.com>
2024-08-12 00:14:03 +0300

    Kyiv photos added.

--- src/components/SideScrollCards.vue
+++ src/components/SideScrollCards.vue
@@ -8,35 +8,16 @@
     <div class="flex justify-center">
       <div class="grid grid-rows-2 grid-cols-2 gap-6 w-full h-full place-items-center">
         <div class="">
-          <img class="max-h-[160px] max-w-[160px]" src="../assets/ukraine_u23.jpg" alt="first picture">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/center_1.jpg" alt="first picture">
         </div>
         <div class="">
-          <img class="max-h-[160px] max-w-[160px]" src="../assets/fc-polissya.png" alt="second picture">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/center_2.jpg" alt="second picture">
         </div>
         <div class="">
-          <img class="max-h-[160px] max-w-[160px]" src="../assets/fc-obolon.jpg" alt="third picture">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/center_3.jpg" alt="third picture">
         </div>
         <div class="">
-          <img class="max-h-[160px] max-w-[160px]" src="../assets/ukraine_u23.jpg" alt="fourth picture">
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="flex justify-center items-center w-1/4 min-w-72 h-2/3 bg-[white] side-scroll-element">
-    <div class="flex justify-center">
-      <div class="grid grid-rows-2 grid-cols-2 gap-6 w-full h-full place-items-center">
-        <div class="">
-          <img class="max-h-[160px] max-w-[160px]" src="../assets/polissya_logo.jpg" alt="first picture">
-        </div>
-        <div class="">
-          <img class="max-h-[160px] max-w-[160px]" src="../assets/fc-polissya.png" alt="second picture">
-        </div>
-        <div class="">
-          <img class="max-h-[160px] max-w-[160px]" src="../assets/fc-polissya.png" alt="third picture">
-        </div>
-        <div class="">
-          <img class="max-h-[160px] max-w-[160px]" src="../assets/polissya_logo.jpg" alt="fourth picture">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/center_4.jpg" alt="fourth picture">
         </div>
       </div>
     </div>
@@ -46,35 +27,16 @@
     <div class="flex justify-center">
       <div class="grid grid-rows-2 grid-cols-2 gap-6 w-full h-full place-items-center">
         <div class="">
-          <img class="max-h-[160px] max-w-[160px]" src="../assets/ukraine_u23.jpg" alt="first picture">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/bricks_1.jpg" alt="first picture">
         </div>
         <div class="">
-          <img class="max-h-[160px] max-w-[160px]" src="../assets/fc-polissya.png" alt="second picture">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/bricks_2.jpg" alt="second picture">
         </div>
         <div class="">
-          <img class="max-h-[160px] max-w-[160px]" src="../assets/fc-obolon.jpg" alt="third picture">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/bricks_3.jpg" alt="third picture">
         </div>
         <div class="">
-          <img class="max-h-[160px] max-w-[160px]" src="../assets/ukraine_u23.jpg" alt="fourth picture">
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="flex justify-center items-center w-1/4 min-w-72 h-2/3 bg-[white] side-scroll-element">
-    <div class="flex justify-center">
-      <div class="grid grid-rows-2 grid-cols-2 gap-6 w-full h-full place-items-center">
-        <div class="">
-          <img class="max-h-[160px] max-w-[160px]" src="../assets/obolon_logo.jpg" alt="first picture">
-        </div>
-        <div class="">
-          <img class="max-h-[160px] max-w-[160px]" src="../assets/fc-obolon.jpg" alt="second picture">
-        </div>
-        <div class="">
-          <img class="max-h-[160px] max-w-[160px]" src="../assets/fc-obolon.jpg" alt="third picture">
-        </div>
-        <div class="">
-          <img class="max-h-[160px] max-w-[160px]" src="../assets/obolon_logo.jpg" alt="fourth picture">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/bricks_4.jpg" alt="fourth picture">
         </div>
       </div>
     </div>
@@ -84,16 +46,73 @@
     <div class="flex justify-center">
       <div class="grid grid-rows-2 grid-cols-2 gap-6 w-full h-full place-items-center">
         <div class="">
-          <img class="max-h-[160px] max-w-[160px]" src="../assets/ukraine_u23.jpg" alt="first picture">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/church_1.jpg" alt="first picture">
         </div>
         <div class="">
-          <img class="max-h-[160px] max-w-[160px]" src="../assets/fc-polissya.png" alt="second picture">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/church_2.jpg" alt="second picture">
         </div>
         <div class="">
-          <img class="max-h-[160px] max-w-[160px]" src="../assets/fc-polissya.png" alt="third picture">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/church_3.jpg" alt="third picture">
         </div>
         <div class="">
-          <img class="max-h-[160px] max-w-[160px]" src="../assets/ukraine_u23.jpg" alt="fourth picture">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/church_4.jpg" alt="fourth picture">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="flex justify-center items-center w-1/4 min-w-72 h-2/3 bg-[white] side-scroll-element">
+    <div class="flex justify-center">
+      <div class="grid grid-rows-2 grid-cols-2 gap-6 w-full h-full place-items-center">
+        <div class="">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/liviy_1.jpg" alt="first picture">
+        </div>
+        <div class="">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/liviy_2.jpg" alt="second picture">
+        </div>
+        <div class="">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/liviy_3.jpg" alt="third picture">
+        </div>
+        <div class="">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/liviy_4.jpg" alt="fourth picture">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="flex justify-center items-center w-1/4 min-w-72 h-2/3 bg-[white] side-scroll-element">
+    <div class="flex justify-center">
+      <div class="grid grid-rows-2 grid-cols-2 gap-6 w-full h-full place-items-center">
+        <div class="">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/bridge_1.jpg" alt="first picture">
+        </div>
+        <div class="">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/bridge_2.jpg" alt="second picture">
+        </div>
+        <div class="">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/bridge_3.jpg" alt="third picture">
+        </div>
+        <div class="">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/bridge_4.jpg" alt="fourth picture">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="flex justify-center items-center w-1/4 min-w-72 h-2/3 bg-[white] side-scroll-element">
+    <div class="flex justify-center">
+      <div class="grid grid-rows-2 grid-cols-2 gap-6 w-full h-full place-items-center">
+        <div class="">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/else_1.jpg" alt="first picture">
+        </div>
+        <div class="">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/else_2.jpg" alt="second picture">
+        </div>
+        <div class="">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/else_3.jpg" alt="third picture">
+        </div>
+        <div class="">
+          <img class="max-h-[210px] max-w-[160px]" src="../assets/else_4.jpg" alt="fourth picture">
         </div>
       </div>
     </div>
